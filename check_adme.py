64c17fed33e75e8315eb94c4944b595e5cedb6fb
barmakmostofian <barmakmostofian@users.noreply.github.com>
2026-04-16 01:31:31 -0400

    Create check_adme.py

---
+++ check_adme.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+from rdkit import Chem
+from rdkit.Chem import Descriptors, rdMolDescriptors
+
+from utils import desirability
+from utils import cns_mpo
+
+
+
+############################################################
+######### Simple CNS MPO (Wager et al. 2010, 2016) #########
+
+file = 'example_compounds.csv'
+df = pd.read_csv(file, sep=',', header=0)
+
+
+# Long version:
+for mol in df['Compound Structure'] :
+     for key, value in result.items():
+        print(f"{key}: {value}")
+     print("\n")
+
+
+# Short version:
+for mol in df['Compound Structure'] :
+    result = cns_mpo(mol)
+    print(mol, result['MPO_score'])
+
+############################################################
